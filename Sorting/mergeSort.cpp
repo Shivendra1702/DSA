d1a43f0d266720e70fd46f7170667446e3cb43e9
Shivendra1702 <shivendrapratapjadia@gmail.com>
2023-08-19 22:13:59 +0530

    19/8/23

--- Sorting/mergeSort.cpp
+++ Sorting/mergeSort.cpp
@@ -1,15 +1,39 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-void merge(int arr[] , int start , int end){
-    int mid = start + (end-start)/2;
+void merge(int arr[] , int start ,int mid , int end){
+    vector<int> temp;
+    int left = start , right = mid+1;
 
-    
+    while(left<=mid && right <= end){
+       if(arr[left]<=arr[right]){
+          temp.push_back(arr[left]);
+          left++;
+       }
+       else{
+          temp.push_back(arr[right]);
+          right++;
+       }
+    }
+
+    while(left<=mid){
+     temp.push_back(arr[left]);
+     left++;  
+    }
+
+    while(right<=end){
+     temp.push_back(arr[right]);
+     right++;
+    }
+
+    for(int i=start ; i<=end ; i++){
+        arr[i]=temp[i-start];
+    }
 }
 
 
 void mergeSort(int arr[] , int start , int end){
-    if(start > end){
+    if(start >= end){
         return ; 
     }
 
@@ -20,8 +44,9 @@ void mergeSort(int arr[] , int start , int end){
 
     //right part
     mergeSort(arr,mid+1,end);
+    // cout<<"haa";
 
-    merge(arr , start , end);
+    merge(arr , start ,mid, end);
 }
 
 int main()           
